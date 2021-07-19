e91a56154b9ce5a2e88d64d2da1e8e875a075fbc
Dio <gank23456@gmail.com>
2021-07-19 20:11:14 +0800

    first

--- app.js
+++ app.js
@@ -18,10 +18,15 @@ const bot = linebot({
 const linebotParser = bot.parser();
 
 bot.on('message', function (event) {
+  event.reply(event.message.text).then(function (data) {
+    // 當訊息成功回傳後的處理
+  }).catch(function (error) {
+    // 當訊息回傳失敗後的處理
+  });
   event.reply({ type: 'text', text: "hehe" }).then((data) => {
 
   }).catch((err) => {
-    
+
   });
   Event.source.member().then((member) => {
     event.reply({ type: 'text', text: "hehe" });
